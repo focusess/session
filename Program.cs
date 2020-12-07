8f17ede855b6e2efedbf4e9f760c513c8c7c6b26
roowdy <47495921+focusess@users.noreply.github.com>
2020-12-07 14:15:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GitHub.com
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class People
+    {
+        Person[] people;
+
+        public People()
+        {
+            people = new Person[2];
+        }
+        public Person this[int index]
+        {
+            get
+            {
+                if (index == 0) return people[0];
+                if (index == 1) return people[1];
+                else
+                {
+                    Console.WriteLine("Введите верный индекс !");
+                    return people[index];
+                }
+            }
+            set
+            {
+                if (index == 0) people[0] = value;
+                if (index == 1) people[1] = value;
+                else Console.WriteLine("Введите верный индекс !");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите индекс");
+            Convert.ToInt32(Console.ReadLine());
+            People people = new People();
+            people[0] = new Person() { Name = "Tom" };
+            people[1] = new Person() { Name = "Bob" };
+            Person x = people[0];
+            Person y = people[1];
+            Console.WriteLine(x?.Name);
+            Console.WriteLine(y?.Name);
+            Console.ReadKey();
+        }
+    }
+}
